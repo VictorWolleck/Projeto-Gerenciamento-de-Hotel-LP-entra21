d493d07f7a7a1be091e25217eb0eb784e60c442a
Guest 01 <guest01@alunos.sc.senac.br>
2022-05-13 21:14:35 -0300

    first commit

--- src/projeto/GerenciamentoDeHotel.java
+++ src/projeto/GerenciamentoDeHotel.java
@@ -9,15 +9,13 @@ public class GerenciamentoDeHotel {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		sc.useDelimiter(System.lineSeparator());
-		int situacao;
-
-		/*
-		 * ArrayList<Integer> roomName = new ArrayList<Integer>(); ArrayList<String>
-		 * roomPosition = new ArrayList<String>(); ArrayList<Character> roomSmoker = new
-		 * ArrayList<Character>(); ArrayList<Integer> roomCapacity = new
-		 * ArrayList<Integer>(); ArrayList<Float> roomDailyRate = new
-		 * ArrayList<Float>();
-		 */
+		int choice;
+		int choiceClientOrRomm = 0;
+		ArrayList<Integer> roomName = new ArrayList<Integer>();
+		ArrayList<String> roomPosition = new ArrayList<String>();
+		ArrayList<String> roomSmoker = new ArrayList<String>();
+		ArrayList<Integer> roomCapacity = new ArrayList<Integer>();
+		ArrayList<Float> roomDailyRate = new ArrayList<Float>();
 
 		ArrayList<String> clientName = new ArrayList<String>();
 		ArrayList<String> clientBirthDate = new ArrayList<String>();
@@ -27,41 +25,107 @@ public class GerenciamentoDeHotel {
 		ArrayList<String> clientUF = new ArrayList<String>();
 		ArrayList<String> clientSmoker = new ArrayList<String>();
 
-		// quarto cadastro
+		// menu inicial
 
-		System.out.println("1- Cadastrar novo Cliente");
+		System.out.println("1- Cadastrar novo Cliente/Quarto");
+		System.out.println("2- Editar e Excluir Cliente/Quarto");
+		System.out.println("3- Listar Clientes/Quartos");
+		System.out.println("4- Realizar Check-in/Check-out");
 		System.out.println("0- Sair");
-		situacao = sc.nextInt();
+		choice = sc.nextInt();
 
-		// add novo quarto
-		
-		switch (situacao) {
-		case 1:
+		switch (choice) {
 
-			System.out.println("Insira o nome do cliente:");
-			clientName.add(sc.next());
+		case 1: // 1- Cadastrar novo Cliente/Quarto
 
-			System.out.println("Insira o e-mail cliente:");
-			clientEmail.add(sc.next());
+			System.out.println("1- Cadastrar Cliente");
+			System.out.println("2- Cadastrar Quarto");
+			System.out.println("0- Retornar");
 
-			System.out.println("Insira a data de nascimento do cliente:");
-			clientBirthDate.add(sc.next());
+			choiceClientOrRomm = sc.nextInt();
 
-			System.out.println("Insira o telefone do cliente:");
-			clientPhone.add(sc.next());
+			switch (choiceClientOrRomm) {
 
-			System.out.println("Insira a cidade do cliente:");
-			clientCity.add(sc.next());
+			case 1:
+				System.out.println("Insira o nome do cliente:");
+				clientName.add(sc.next());
 
-			System.out.println("Insira estado do cliente:");
-			clientUF.add(sc.next());
+				System.out.println("Insira o e-mail cliente:");
+				clientEmail.add(sc.next());
 
-			System.out.println("Cliente fumante? S/N:");
-			clientSmoker.add(sc.next());
+				System.out.println("Insira a data de nascimento do cliente:");
+				clientBirthDate.add(sc.next());
 
-			break;
+				System.out.println("Insira o telefone do cliente:");
+				clientPhone.add(sc.next());
 
-		case 2:
+				System.out.println("Insira a cidade do cliente:");
+				clientCity.add(sc.next());
+
+				System.out.println("Insira estado do cliente:");
+				clientUF.add(sc.next());
+
+				System.out.println("Cliente fumante? S/N:");
+				clientSmoker.add(sc.next());
+
+				System.out.println("Cliente Cadastrado!");
+
+				break;
+
+			case 2:
+				System.out.println("Insira o nome do quarto:");
+				roomName.add(sc.nextInt());
+
+				System.out.println("Insira o e-mail cliente:");
+				roomPosition.add(sc.next());
+
+				System.out.println("Insira a data de nascimento do cliente:");
+				roomSmoker.add(sc.next());
+
+				System.out.println("Insira o telefone do cliente:");
+				roomCapacity.add(sc.nextInt());
+
+				System.out.println("Insira a cidade do cliente:");
+				roomDailyRate.add(sc.nextFloat());
+
+			case 0:
+				System.out.println("Retornando");
+				break;
+
+			default:
+				System.out.println("Opção Inválida!");
+
+			}
+
+		case 2: // 2- Editar e Excluir Cliente/Quarto
+			
+		case 3: // 3- Listar Clientes/Quartos
+
+			
+			System.out.println("1- Listar Clientes");
+			System.out.println("2- Listar Quartos");
+			System.out.println("0- Retornar");
+			int listClientsOrRooms = sc.nextInt();
+			switch (listClientsOrRooms) {
+
+			case 1:
+				System.out.println(clientName.toString()); 
+				return;
+
+			case 2:
+				System.out.println(roomName.toString());
+
+			case 0:
+				System.out.println("Retornando");
+				break;
+
+			default:
+				System.out.println("Opção Inválida!");
+
+			}
+
+		case 4: // 4- Realizar Check-in/Check-out
+		case 0: // 0- Sair
 
 			System.out.println("Saiu");
 
@@ -72,11 +136,7 @@ public class GerenciamentoDeHotel {
 }
 
 /*
- * 
- * Olá,
- * 
- * Segue o desafio para quem deseja focar no desafio e vou direcionando o
- * conhecimento da disciplina.
+ *
  * 
  * Criar um sistema de hotel onde permita: Cadastrar, Editar, Listar e Excluir
  * Clientes; Cadastrar, Editar, Listar e Excluir Quartos; Verificar os quartos
